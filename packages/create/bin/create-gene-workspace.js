5ce7a7576a41933ca7d6d996d62cd1ed78fd97cf
Karol Rzotki <karol.rzotki@brainly.com>
2024-11-18 12:32:22 +0100

    fix(init): Get packages versions from package.json

--- packages/create/bin/create-gene-workspace.js
+++ packages/create/bin/create-gene-workspace.js
@@ -4,19 +4,15 @@ const { spawn } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-// Function to read the NX version from package.json
-function getNxVersion() {
+// Function to read dependencies and devDependencies from package.json
+function getPackageVersion(packageName) {
   const packageJsonPath = path.resolve(__dirname, '..', 'package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-  return packageJson.dependencies && packageJson.dependencies['@nx/workspace']
-    ? packageJson.dependencies['@nx/workspace']
-    : null;
-}
 
-function getCurrentPackageVersion() {
-  const packageJsonPath = path.resolve(__dirname, '..', 'package.json');
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-  return packageJson.version;
+  const dependencies = packageJson.dependencies || {};
+  const devDependencies = packageJson.devDependencies || {};
+
+  return dependencies[packageName] || devDependencies[packageName] || 'latest';
 }
 
 // Function to execute a command and display its output in real-time
@@ -45,18 +41,11 @@ if (!name) {
   process.exit(1);
 }
 
-// Get the NX version from package.json
-const nxVersion = getNxVersion();
+// Retrieve the NX version
+const nxVersion = getPackageVersion('@nx/workspace');
 
-if (!nxVersion) {
+if (nxVersion === 'latest') {
   console.error('NX version not found in package.json.');
-  process.exit(1);
-}
-
-const packageVersion = getCurrentPackageVersion();
-
-if (!packageVersion) {
-  console.error('Package version not found in package.json.');
   process.exit(1);
 }
 
@@ -66,7 +55,6 @@ console.log(`Creating workspace ${name} with NX version ${nxVersion}...`);
 async function runCommands() {
   try {
     // Use the NX version and name argument in the command
-
     await execCommand('npx', [
       `create-nx-workspace@${nxVersion}`,
       `--name=${name}`,
@@ -85,65 +73,74 @@ async function runCommands() {
 
     console.log('Installing dependencies with pnpm');
 
-    // Install dependencies
-    await execCommand('pnpm', [
-      'install',
-      `@brainly-gene/core@${packageVersion}`,
-      `@brainly-gene/next@${packageVersion}`,
-      `next@12.3.4`,
-      `ramda@0.30.1`,
-      `react@18.3.1`,
-      `react-dom@18.3.1`,
+    // List of dependencies to install
+    const dependencies = [
+      '@brainly-gene/core',
+      '@brainly-gene/next',
+      'next',
+      'ramda',
+      'react',
+      'react-dom',
       'brainly-style-guide',
-      `inversify@5.1.1`,
-      `@tanstack/react-query@5.55.4`,
-      '@nx/next@15.8.6',
-      'style-loader@2.0.0',
-      'css-loader@3.6.0',
-      'sass@1.55.0',
-      'sass-loader@9.0.2',
-      'classnames@2.5.1',
-    ]);
+      'inversify',
+      '@tanstack/react-query',
+      '@nx/next',
+      'style-loader',
+      'css-loader',
+      'sass',
+      'sass-loader',
+      'classnames',
+    ];
 
-    // Install dev dependencies
-    await execCommand('pnpm', [
-      'install',
-      '-D',
-      `@brainly-gene/tools@${packageVersion}`,
-      `@brainly-gene/eslint-plugin@${packageVersion}`,
-      `eslint@8.44.0`,
-      `@types/react@18.3.12`,
-      `@types/ramda@0.30.2`,
-      `@types/node@18.19.55`,
-      'eslint-plugin-cypress@2.10.3',
-      '@nx/eslint-plugin@15.8.9',
-      'eslint-plugin-react-hooks@4.6.0',
-      'eslint-plugin-jsx-a11y@6.6.1',
-      'eslint-plugin-react@7.31.11',
-      'eslint-config-next@13.1.1',
-      '@nx/jest@15.8.9',
-      'msw@0.49.2',
-      'msw-storybook-addon@1.6.3',
-      '@storybook/addon-actions@6.5.16',
-      '@storybook/core-server@6.5.16',
-      '@storybook/addon-backgrounds@6.3.8',
-      '@storybook/addon-knobs@6.3.1',
-      '@storybook/addon-links@6.3.8',
-      '@storybook/addon-storysource@6.3.8',
-      '@storybook/addon-viewport@6.3.8',
-      '@storybook/addons@6.3.8',
-      '@storybook/builder-webpack5@6.5.16',
-      '@storybook/manager-webpack5@6.5.16',
-      '@storybook/react@6.5.16',
-      '@storybook/theming@6.5.16',
-      '@testing-library/dom@8.20.1',
-      '@testing-library/jest-dom@6.2.1',
-      '@testing-library/react@16.0.0',
-      '@nx/cypress@15.8.9',
-      '@nx/react@15.8.9',
-      '@nx/web@15.8.9',
-      '@types/jest@29.4.4',
-    ]);
+    const dependencyArgs = dependencies.map(
+      (pkg) => `${pkg}@${getPackageVersion(pkg)}`
+    );
+    await execCommand('pnpm', ['install', ...dependencyArgs]);
+
+    // List of devDependencies to install
+    console.log('Installing dev dependencies with pnpm');
+    const devDependencies = [
+      '@brainly-gene/tools',
+      '@brainly-gene/eslint-plugin',
+      'eslint',
+      '@types/react',
+      '@types/ramda',
+      '@types/node',
+      'eslint-plugin-cypress',
+      '@nx/eslint-plugin',
+      'eslint-plugin-react-hooks',
+      'eslint-plugin-jsx-a11y',
+      'eslint-plugin-react',
+      'eslint-config-next',
+      '@nx/jest',
+      'msw',
+      'msw-storybook-addon',
+      '@storybook/addon-actions',
+      '@storybook/core-server',
+      '@storybook/addon-backgrounds',
+      '@storybook/addon-knobs',
+      '@storybook/addon-links',
+      '@storybook/addon-storysource',
+      '@storybook/addon-viewport',
+      '@storybook/addons',
+      '@storybook/builder-webpack5',
+      '@storybook/manager-webpack5',
+      '@storybook/react',
+      '@storybook/theming',
+      '@testing-library/dom',
+      '@testing-library/jest-dom',
+      '@testing-library/react',
+      '@nx/cypress',
+      '@nx/react',
+      '@nx/web',
+      '@types/jest',
+      'cypress'
+    ];
+
+    const devDependencyArgs = devDependencies.map(
+      (pkg) => `${pkg}@${getPackageVersion(pkg)}`
+    );
+    await execCommand('pnpm', ['install', '-D', ...devDependencyArgs]);
 
     console.log('Generating gene-workspace with pnpm nx');
     await execCommand('pnpm', [
