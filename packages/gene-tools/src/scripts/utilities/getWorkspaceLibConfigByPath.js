0a2c9f2703921379dbbb826036ea691913387169
RaulVallespinLuque <raul.vallespinluque@brainly.com>
2025-04-09 12:11:26 +0200

    fix(gene-tools): add getWorkspaceLibConfigByPath utility function

---
+++ packages/gene-tools/src/scripts/utilities/getWorkspaceLibConfigByPath.js
@@ -0,0 +1,12 @@
+const { readWorkspaceConfig } = require('@nx/workspace');
+const fs = require('fs');
+
+function getWorkspaceLibConfigByPath(path) {
+  const { projects } = readWorkspaceConfig({ format: 'nx' });
+
+  return Object.values(projects).find((project) => {
+    return path.startsWith(project.sourceRoot);
+  });
+}
+
+module.exports = { getWorkspaceLibConfigByPath };
