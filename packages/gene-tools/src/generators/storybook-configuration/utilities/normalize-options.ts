a4e9fb6d3ef148134b197701d6f8c20a31d518fd
Ihor <kopach@users.noreply.github.com>
2025-06-06 12:00:30 +0200

    --wip--

--- packages/gene-tools/src/generators/storybook-configuration/utilities/normalize-options.ts
+++ packages/gene-tools/src/generators/storybook-configuration/utilities/normalize-options.ts
@@ -1,23 +1,29 @@
 import type { Tree } from '@nx/devkit';
-import { getProjects } from '@nx/devkit';
+// import { getProjects } from '@nx/devkit';
 import type { NormalizedOptions, Options } from '../schema';
 
 export function normalizeOptions(
   tree: Tree,
   options: Options,
 ): NormalizedOptions {
-  const projects = getProjects(tree);
-  const project = projects.get(options.name);
-  if (!project) {
-    throw new Error(
-      `The project "${options.name}" does not exist in the workspace.`,
-    );
-  }
-
   return {
     ...options,
-    isLibrary: project.projectType === 'library',
-    projectRoot: project.root,
-    storybookDir: `${project.root}/.storybook`,
+    isLibrary: false,
+    projectRoot: '',
+    storybookDir: '',
   };
+  // const projects = getProjects(tree);
+  // const project = projects.get(options.name);
+  // if (!project) {
+  //   throw new Error(
+  //     `The project "${options.name}" does not exist in the workspace.`,
+  //   );
+  // }
+
+  // return {
+  //   ...options,
+  //   isLibrary: project.projectType === 'library',
+  //   projectRoot: project.root,
+  //   storybookDir: `${project.root}/.storybook`,
+  // };
 }
