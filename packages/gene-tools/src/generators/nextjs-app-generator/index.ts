c05ff6a6fc8053bb1930b85a202d608bd51263b3
Ihor <kopach@users.noreply.github.com>
2025-06-09 13:42:27 +0200

    chore: update generator for nextjs-app

--- packages/gene-tools/src/generators/nextjs-app-generator/index.ts
+++ packages/gene-tools/src/generators/nextjs-app-generator/index.ts
@@ -4,7 +4,6 @@ import {
   installPackagesTask,
   generateFiles,
   joinPathFragments,
-  getWorkspaceLayout,
   offsetFromRoot,
   readJson,
   writeJson,
@@ -24,11 +23,11 @@ import { excludeTestsBoilerplate } from './utils/excludeTestsBoilerplate';
 import { updateCypressTsConfig } from '../utilities';
 
 export default async function (tree: Tree, schema: BrainlyNextJSAppGenerator) {
-  const { name, directory = '', e2e } = schema;
+  const { name: projectName, directory = '', e2e } = schema;
   const currentPackageJson = readJson(tree, 'package.json');
 
   await applicationGenerator(tree, {
-    name: name,
+    name: projectName,
     directory: directory,
     tags: resolveTags(schema),
     style: 'none',
@@ -40,22 +39,18 @@ export default async function (tree: Tree, schema: BrainlyNextJSAppGenerator) {
     src: false,
   });
 
+  const appDir = directory;
   const normalizedDirectory = directory.replace(/\//g, '-');
-  const projectName = normalizedDirectory
-    ? `${normalizedDirectory}-${name}`
-    : name;
-  const projectPath = `${directory}/${name}`;
+
   await updateWorkspaceTarget({
     tree,
-    projectPath,
+    projectPath: appDir,
     projectName,
     e2e,
     directory: normalizedDirectory,
   });
 
-  const { appsDir } = getWorkspaceLayout(tree);
-  const appDir = `${appsDir}/${projectPath}`;
-  const e2eDir = `${appsDir}/${projectPath}-e2e`;
+  const e2eDir = `${directory}-e2e`;
   const initialPage = 'Home';
 
   generateFiles(tree, joinPathFragments(__dirname, './files/app'), appDir, {
