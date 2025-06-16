d733b6d24175e0aaca9c9cd873ef14aa76f2620f
Ihor <kopach@users.noreply.github.com>
2025-06-16 14:34:51 +0200

    chore: update translation library generator unit tests

--- packages/gene-tools/src/generators/translations-library-generator/index.spec.ts
+++ packages/gene-tools/src/generators/translations-library-generator/index.spec.ts
@@ -2,6 +2,7 @@ import type { Tree } from '@nx/devkit';
 import { logger } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
 import translationsLibraryGenerator from './index';
+import { nxFileTreeSnapshotSerializer } from '../core-module-generator/utils/nxFileTreeSnapshotSerializer';
 
 describe('Translations library generator', () => {
   let appTree: Tree;
@@ -16,8 +17,34 @@ describe('Translations library generator', () => {
   it('should generate files', async () => {
     await translationsLibraryGenerator(appTree);
 
-    expect(
-      appTree.exists('libs/translations/src/locales/test.json'),
-    ).toBeTruthy();
+    expect(nxFileTreeSnapshotSerializer(appTree)).toMatchInlineSnapshot(`
+      ".prettierrc
+      package.json
+      nx.json
+      tsconfig.base.json
+      apps
+      └── .gitignore
+      libs
+      ├── .gitignore
+      └── translations
+          ├── project.json
+          ├── README.md
+          ├── src
+          │   ├── index.ts
+          │   └── locales
+          │       └── test.json
+          ├── tsconfig.lib.json
+          ├── .babelrc
+          ├── tsconfig.json
+          ├── .eslintrc.json
+          ├── tsconfig.spec.json
+          └── jest.config.ts
+      .prettierignore
+      .eslintrc.json
+      .eslintignore
+      jest.preset.js
+      jest.config.ts
+      "
+    `);
   });
 });
