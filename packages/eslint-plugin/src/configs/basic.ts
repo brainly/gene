a69808475f0c01bad225178c96fab30c5ad4bd16
Karol Rzotki <karol.rzotki@brainly.com>
2025-09-05 19:16:17 +0200

    feat(eslint-plugin): include 'type:native-module' in basic config

--- packages/eslint-plugin/src/configs/basic.ts
+++ packages/eslint-plugin/src/configs/basic.ts
@@ -57,6 +57,7 @@ export default {
               'type:utility',
               'type:machine',
               'type:vendor',
+              'type:native-module'
             ],
           },
           {
